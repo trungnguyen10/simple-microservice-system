58a2de07d48a8050179845bd5e469d5a163cf625
Trung Nguyen <trungnguyen2809@gmail.com>
2022-08-19 11:10:34 -0500

    add event-bus service

---
+++ event-bus/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {
+  const event = req.body;
+
+  axios.post('http://localhost:4000/events', event).catch((err) => {
+    console.log('error sending to localhost:4000', err);
+  });
+  axios.post('http://localhost:4001/events', event).catch((err) => {
+    console.log('error sending to localhost:4001', err);
+  });
+  axios.post('http://localhost:4002/events', event).catch((err) => {
+    console.log('error sending to localhost:4002', err);
+  });
+
+  res.send({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+  console.log('event-bus is listening on port 4005');
+});
