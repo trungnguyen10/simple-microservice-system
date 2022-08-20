e32ba2950d11c77eb388b32b71fb9ce37e68f233
Trung Nguyen <trungnguyen2809@gmail.com>
2022-08-19 19:21:40 -0500

    implement event sync

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,13 +13,7 @@ app.use(
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
-
+function handleEvent(type, data) {
   if (type === 'PostCreated') {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
@@ -37,10 +32,28 @@ app.post('/events', (req, res) => {
       comment[property] = data[property];
     }
   }
+}
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
 
   res.sendStatus(200);
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('query service is listening on port 4002');
+
+  const res = await axios.get('http://localhost:4005/events');
+
+  for (let event of res.data) {
+    console.log('Processing event: ', event.type);
+
+    handleEvent(event.type, event.data);
+  }
 });
